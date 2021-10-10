ee4840838098affad7ab9080cf977b4a19324074
Isiah Jones <isiah.jones33@outlook.com>
2021-10-10 10:55:54 -0400

    Configure Startup.cs

--- BlogProject/Startup.cs
+++ BlogProject/Startup.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PersonalBlog.Data;
+using PersonalBlog.Models;
+using PersonalBlog.Services;
+using PersonalBlog.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +29,59 @@ namespace BlogProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Connection String - driver determines which backend
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql( //driver
+                    ConnectionService.GetConnectionString(Configuration))); //goes to appsettings.json file to get DefaultConnection
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+            //Updated to register service to BlogUser
+            services.AddIdentity<BlogUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                //Services Injected
+                .AddDefaultUI()
+                .AddDefaultTokenProviders()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
+
+            //Added Razorpages
+            services.AddRazorPages();
+
+            //Register Custom DataService class
+            services.AddScoped<DataService>();
+
+            // Register Search Service
+            services.AddScoped<BlogSearchService>();
+
+            //Register a pre configured instance of MailSettings class
+            services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
+            services.AddScoped<IBlogEmailSender, EmailService>();
+
+            //Register Image Service
+            services.AddScoped<IImageService, BasicImageService>();
+
+            //Register Slug Service
+            services.AddScoped<ISlugService, BasicSlugService>();
+
+            //Modify the default settings for password requirements
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequiredUniqueChars = 0;
+            });
+        }
+
+        private Action<object> MailSettings()
+        {
+            throw new NotImplementedException();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +90,7 @@ namespace BlogProject
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
             }
             else
             {
@@ -44,13 +103,31 @@ namespace BlogProject
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "test",
+                    pattern: "Categories/",
+                    defaults: new { controller = "Blogs", action = "Index" });
+
+                endpoints.MapControllerRoute(
+                    name: "SlugRouteBlog",
+                    pattern: "Categories/{slug}",
+                    defaults: new { controller = "Blogs", action = "Details" });
+
+                endpoints.MapControllerRoute(
+                    name: "SlugRoutePost",
+                    pattern: "Categories/{blog}/{slug}",
+                    defaults: new { controller = "Posts", action = "Details" });
+
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
